3cc24794e5e5f5c234c9d567583d51aa8d1aee7d
toriumi <na54heart@icloud.com>
2019-03-22 00:09:21 +0900

    Load sound sample

--- practice/main.js
+++ practice/main.js
@@ -1,3 +1,4 @@
+// 音声を管理する役割を持つ AudioContext を作成する
 var context;
 window.addEventListener('load','init', false);
 function init() {
@@ -8,4 +9,23 @@ function init() {
     catch (e) { // exception_var の 'e'
         alert('Web Audio API is not supported in this browser.');
     }
+}
+
+// 音声サンプルをロードする
+var dogBarkingBuffer = null;
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+
+function loadDogSound(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    // Decode asynchronously
+    request.onload = function () {
+        context.decodeAudioData(request.response, function (buffer) {
+            dogBarkingBuffer = buffer;
+        }, onError);
+    };
+    request.send();
 }
