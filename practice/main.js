f712612470fedd080023b55c31a5e709649d80d5
toriumi <na54heart@icloud.com>
2019-03-22 00:40:42 +0900

    Web Audio API abstraction

--- practice/main.js
+++ practice/main.js
@@ -32,7 +32,7 @@ function loadDogSound(url) {
 
 // 音を再生する
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
-const context = new AudioContext();
+var context = new AudioContext();
 
 function playSound(buffer) {
     const source = context.createBufferSource(); // create a sound source
@@ -40,4 +40,35 @@ function playSound(buffer) {
     source.connect(context.destination); // connect the source now
     source.start(0); // play the source now
     // note: on older systems, may have to use deprecated noteOn(time);
+}
+
+window.onload = init;
+var context;
+var bufferLoader;
+
+function init() {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    constext = new AudioContext();
+
+    bufferLoader = new BufferLoader(
+        context,
+        [
+            '../sounds/hyper-reality/br-jam-loop.wav',
+            '../sounds/hyper-reality/laughter.wav'
+        ],
+        finishedLoading
+    );
+    bufferLoader.load();
+}
+
+function finishedLoading(bufferList) {
+    var source1 = context.createBufferSource();
+    var source2 = context.createBufferSource();
+    source1.buffer = bufferList[0];
+    source2.buffer = bufferList[1];
+
+    source1.connect(context.destination);
+    source2.connect(context.destination);
+    source1.start(0);
+    source2.start(0);
 }
